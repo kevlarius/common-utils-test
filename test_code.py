74eab7efc39efbb1a51e58db7993bb91b406ad22
Igor Pankov <igor9mail@gmail.com>
2019-08-25 19:34:00 +0300

    Init commit for test repo
    Init commit for test repo

---
+++ test_code.py
@@ -0,0 +1,7 @@
+
+from utils.strings import compare_strings
+
+
+if __name__ == '__main__':
+    result = compare_strings('abcd', 'abcd')
+    print(result)
---
+++ utils/strings.py
@@ -0,0 +1,4 @@
+
+
+def compare_strings(str1, str2):
+    return str1 == str2
